b368052fcd2bf9b6b40acc3a0b2300c1d84d2334
Tobias Keil <tkeil@2-D-8.42heilbronn.de>
2024-11-08 15:26:16 +0100

    updated ft_putendl_fd.c for managing NULLs

--- libft/ft_putendl_fd.c
+++ libft/ft_putendl_fd.c
@@ -2,9 +2,12 @@
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   ft_putendl_fd.c                                    :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: tkeil <tkeil@student.42.fr>                +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
+/*                                                    +:+ +:+        
+	+:+     */
+/*   By: tkeil <tkeil@student.42.fr>                +#+  +:+      
+	+#+        */
+/*                                                +#+#+#+#+#+  
+	+#+           */
 /*   Created: 2024/10/08 13:04:34 by tkeil             #+#    #+#             */
 /*   Updated: 2024/10/08 13:04:34 by tkeil            ###   ########.fr       */
 /*                                                                            */
@@ -12,10 +15,14 @@
 
 #include "libft.h"
 
+
 void	ft_putendl_fd(char *s, int fd)
 {
-	ft_putstr_fd(s, fd);
-	ft_putchar_fd('\n', fd);
+	if (s)
+	{
+		ft_putstr_fd(s, fd);
+		ft_putchar_fd('\n', fd);
+	}
 }
 
 // int main(void)
